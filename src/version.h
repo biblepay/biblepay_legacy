0b67f613b36cec1126e647162f84d19e1b264284
biblepay <contact@biblepay.org>
2017-12-17 09:37:12 -0600

    1.0.6.7b-MANDATORY UPGRADE
    - Update min_peer_version

--- src/version.h
+++ src/version.h
@@ -19,7 +19,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70711;
+static const int MIN_PEER_PROTO_VERSION = 70710;
 static const int MIN_PEER_TESTNET_PROTO_VERSION = 70711;
 
 //! nTime field added to CAddress, starting with this version;
